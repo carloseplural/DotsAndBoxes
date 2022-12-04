0295d21df697c2328e2b87b036c9866f52d19a6e
Carlovila <carlos.gvferreira@gmail.com>
2022-12-04 01:30:10 +0000

    Board, example, some player functions

--- classes.py
+++ classes.py
@@ -1,30 +1,121 @@
-import pandas as pd
+# Board class
 
-class Board():
-    """initialises the board as an empty array"""
+class Board:
+
+    "Initialises the board as an empty array"
     def __init__(self):
         self.board = []
+        self.columns = ['# ']
+        self.rows = []
 
-      """creates the board based on a given input"""
-    def create_board(self, size_selection):
+    "Creates the board based on a given input size"
+    def create_board(self, size_selection, custom=0):
+        "Dictionary that determines the size of the board"
         board_sizes = {
-            "small": 4,
-            "medium": 10,
-            "large": 15
+            'small': 4,
+            'medium': 10,
+            'large': 15,
+            'custom': custom
         }
-        if size_selection == "custom":
-            custom = int(input("Choose a size x (x by x): "))
-            board_sizes["custom"] = custom
-        for r in range(board_sizes[size_selection]):
-            self.board.append(r + 1)
-            self.board.append("")
-        self.board = pd.DataFrame(data="", columns=self.board, index=self.board)
-        for row in range(1, board_sizes[size_selection] + 1):
-            for col in range(1, board_sizes[size_selection] + 1):
-                self.board.loc[row, col] = "+"
 
+        "Creates cells in the board"
+        board = self.board
+        selection = board_sizes[size_selection]
+        for r in range(selection):
+            board.append(['+', ' '] * (selection - 1) + ['+'])
+            board.append([' '] * 7)
+
+        "Creates column index"
+        columns = self.columns
+        for col in range(selection):
+            columns.append(col)
+            if col < (selection - 1) and col < 10:
+                columns.append(' ')
+            elif (selection - 1) > col >= 10:
+                columns.append('')
+
+        "Creates row index"
+        rows = self.rows
+        for row in range(selection):
+            rows.append(row)
+            if row < (selection - 1):
+                rows.append(' ')
+
+    "Displays the board"
     def display_board(self):
-        """"""
-        print(self.board)
+        board = self.board
+        rows = self.rows
+        columns = self.columns
 
-    def
+        "Creates copy to be displayed as to not change original board"
+        board_copy = board.copy()
+
+        "Creates indexes with correct spacing to each row"
+        for row in range(len(rows)):
+            board_copy[row] = [rows[row]] + ([''] * (2 - len(str(rows[row])))) + board_copy[row]
+
+        "Adds columns indexes with correct spacing"
+        for col in columns:
+            print(col, end=' ')
+        print()
+
+        "Adds row/cells to the board"
+        for row in board_copy:
+            for col in row:
+                print(col, end=' ')
+            print()
+
+    "Displays example on a 4x4 board"
+    def example(self):
+        self.create_board("small")
+        example = self.board.copy()
+        print("-" * 16)
+        example[0][2] = "o"
+        example[0][4] = "o"
+        example[0][3] = "—"
+        self.display_board()
+        print("-" * 16)
+
+    "Cell not empty rule"
+    def is_cell_empty(self, cell):
+
+        "If the cell has a 'o' instead of '+', it cannot be played"
+        if self.board[cell[0] * 2][cell[1] * 2] != "+":
+            print("Not valid. Cell is not empty.")
+            return False
+        "Return true if is empty"
+        return True
+
+    "Fill box"
+
+    "Point counter"
+
+    "Reset game"
+
+    "End game"
+
+"Player class"
+class Player:
+
+    "---"
+    def __init__(self):
+        pass
+
+    "Moves the selected player"
+    def move_player(self, player, cell_1, cell_2, board):
+        "logs the move"
+        board.board[cell_1[0] * 2][cell_1[1] * 2] = "o"
+        board.board[cell_2[0] * 2][cell_2[1] * 2] = "o"
+
+        "can only move if points are horizontally or diagonally adjacent"
+        if cell_1[0] != cell_2[0] and cell_1[1] != cell_2[1]:
+            print("Not valid. Cells must be adjacent")
+            return False
+        elif (int(cell_1[0]) - int(cell_2[0])) not in range(-1, 2) or (int(cell_1[1]) - int(cell_2[1])) not in range(-1, 2):
+            print("Not valid. Cells must be adjacent")
+            return False
+        else:
+            return True
+
+    # Player vs computer
+
--- main.py
+++ main.py
@@ -1,15 +1,68 @@
+# Importing classes from the classes.py file
 from classes import Board
+from classes import Player
+import ast
+import random
 
+"Input desired board size"
+range_list = ["small", "medium", "large", "custom"] + [str(r) for r in range(3, 26)]
+size_selection = input("Enter board size \nSmall (4x4), Medium (9x9), Large (15x15) or Custom (x by x, min=3, max=25): ")
+
+"Input must be a valid range"
+while size_selection not in range_list:
+    print("Not valid")
+    size_selection = input("Enter board size \nmall (4x4), Medium (9x9), Large (15x15) or Custom (x by x, min=3, max=25): ").lower()
+
+"Select custom size if option is chosen"
+if size_selection not in ["small", "medium", "large"]:
+    if size_selection == "custom":
+        custom = int(input("Choose size x (x by x): "))
+        while custom not in list(range(3, 26)):
+            print("Not valid")
+            custom = int(input("Choose size x (x by x): "))
+    else:
+        custom = int(size_selection)
+        size_selection = "custom"
+else:
+    custom = 0
+
+"Show game example"
+print("\nChoose two cardinal points to make a move (x1, y1) (x2, y2)", "Example: (0,1) (0,2)", sep="\n")
+example = Board()
+example.example()
+
+"Creates player list"
+player_A = "A"
+player_B = "B"
+player_list = [player_A, player_B]
+p1 = Player()
+p2 = Player()
+
+"Run the game"
 while True:
-    # Input desired board size
-    size_selection = input("Enter board size \nsmall (4x4), medium (9x9), large (15x15) or custom): ").lower()
-    # If input is invalid, enter again
-    while size_selection not in ["small", "medium", "large", "custom"]:
-        print("Not valid")
-        size_selection = input("Enter board size \nsmall (4x4), medium (9x9), large (15x15) or custom): ").lower()
-
-    # Generates the game board
+    "Generates the game board"
     board = Board()
-    board.create_board(size_selection)
+    board.create_board(size_selection="small", custom="custom")
     board.display_board()
-    break
+
+    "Randomizes first move"
+    random.shuffle(player_list)
+    player_1 = player_list[0]
+    player_2 = player_list[1]
+    print(f"Player {player_1} moves first!")
+
+    # while True:
+    #     "Logs move for each player in tuples"
+    #     try:
+    #         cell_1 = ast.literal_eval(input("Enter first point 'x1, y1': "))
+    #         while not board.is_cell_empty(cell=cell_1):
+    #             cell_1 = ast.literal_eval(input("Enter first point 'x1, y1': "))
+    # break
+
+
+
+
+
+
+
+
