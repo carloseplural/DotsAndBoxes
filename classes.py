509be714812f048912d07250a0e018341e17e8b8
Carlovila <carlos.gvferreira@gmail.com>
2022-12-11 13:33:19 +0000

    Fill box, rules, point counter, reset game,  error handling

--- classes.py
+++ classes.py
@@ -1,26 +1,28 @@
-# Board class
-
+"""Creating board class"""
 class Board:
 
-    "Initialises the board as an empty array"
+    "Initialises the board variables"
     def __init__(self):
+
         self.board = []
+        self.size_selection = None
         self.columns = ['# ']
         self.rows = []
+        self.play = "o"
+        self.h_line = "—"
+        self.v_line = "|"
+        self.score_player_A = 0
+        self.score_player_B = 0
+        self.play_count = 0
 
     "Creates the board based on a given input size"
-    def create_board(self, size_selection, custom=0):
-        "Dictionary that determines the size of the board"
-        board_sizes = {
-            'small': 4,
-            'medium': 10,
-            'large': 15,
-            'custom': custom
-        }
+    def create_board(self, size_selection):
+        self.size_selection = int(size_selection)
+        selection = self.size_selection
 
-        "Creates cells in the board"
+        "Populates the board"
         board = self.board
-        selection = board_sizes[size_selection]
+
         for r in range(selection):
             board.append(['+', ' '] * (selection - 1) + ['+'])
             board.append([' '] * 7)
@@ -67,7 +69,8 @@ class Board:
 
     "Displays example on a 4x4 board"
     def example(self):
-        self.create_board("small")
+        print("\nChoose two cardinal points to make a move (x1, y1) (x2, y2)", "Example: (0,1) (0,2)", sep="\n")
+        self.create_board(size_selection=4)
         example = self.board.copy()
         print("-" * 16)
         example[0][2] = "o"
@@ -76,46 +79,236 @@ class Board:
         self.display_board()
         print("-" * 16)
 
-    "Cell not empty rule"
-    def is_cell_empty(self, cell):
+    "Displays score"
+    def get_score(self):
+        print("-" * 20)
+        print(f"Score: A - {self.score_player_A} | B - {self.score_player_B}")
+        print("-" * 20)
 
-        "If the cell has a 'o' instead of '+', it cannot be played"
-        if self.board[cell[0] * 2][cell[1] * 2] != "+":
-            print("Not valid. Cell is not empty.")
+    "Cells are not empty rule"
+    def cells_are_empty(self, cell_1, cell_2):
+
+        "Format cell 1"
+        cell_1_point_1 = cell_1[0] * 2
+        cell_1_point_2 = cell_1[1] * 2
+
+        "Format cell 2"
+        cell_2_point_1 = cell_2[0] * 2
+        cell_2_point_2 = cell_2[1] * 2
+
+        "If target cell has a '-' or '|' it cannot be played"
+        if cell_1_point_1 == cell_2_point_1:
+            if self.board[cell_1_point_1][max(cell_1_point_2, cell_2_point_2) - 1] == self.h_line:
+                return False
+            return True
+        if cell_1_point_2 == cell_2_point_2:
+            if self.board[max(cell_1_point_1, cell_2_point_1) - 1][cell_1_point_2] == self.v_line:
+                return False
+            return True
+
+    "Rule stating that cells must be adjacent"
+    def cells_are_adjacent(self, cell_1, cell_2):
+
+        "Format cell 1"
+        cell_1_point_1 = cell_1[0] * 2
+        cell_1_point_2 = cell_1[1] * 2
+
+        "Format cell 2"
+        cell_2_point_1 = cell_2[0] * 2
+        cell_2_point_2 = cell_2[1] * 2
+
+        "can only move if cells are horizontally or vertically adjacent"
+        if cell_1_point_1 != cell_2_point_1 and cell_1_point_2 != cell_2_point_2:
             return False
-        "Return true if is empty"
-        return True
-
-    "Fill box"
-
-    "Point counter"
-
-    "Reset game"
-
-    "End game"
-
-"Player class"
-class Player:
-
-    "---"
-    def __init__(self):
-        pass
-
-    "Moves the selected player"
-    def move_player(self, player, cell_1, cell_2, board):
-        "logs the move"
-        board.board[cell_1[0] * 2][cell_1[1] * 2] = "o"
-        board.board[cell_2[0] * 2][cell_2[1] * 2] = "o"
-
-        "can only move if points are horizontally or diagonally adjacent"
-        if cell_1[0] != cell_2[0] and cell_1[1] != cell_2[1]:
-            print("Not valid. Cells must be adjacent")
+        elif (int(cell_1_point_1) - int(cell_2_point_1)) not in range(-2, 3):
             return False
-        elif (int(cell_1[0]) - int(cell_2[0])) not in range(-1, 2) or (int(cell_1[1]) - int(cell_2[1])) not in range(-1, 2):
-            print("Not valid. Cells must be adjacent")
+        elif (int(cell_1_point_2) - int(cell_2_point_2)) not in range(-2, 3):
             return False
         else:
             return True
 
-    # Player vs computer
+    "Cells must be different"
+    def cells_are_different(self, cell_1, cell_2):
+        if cell_1 == cell_2:
+            return False
+        else:
+            return True
 
+    "Check all the rules"
+    def check_rules(self, cell_1, cell_2):
+        if not self.cells_are_different(cell_1, cell_2):
+            print("Cells must be different. ", end="")
+            return False
+        elif not self.cells_are_adjacent(cell_1,cell_2):
+            print("Cells must be adjacent. ", end="")
+            return False
+        elif not self.cells_are_empty(cell_1, cell_2):
+            print("This move has already been played. ", end="")
+        else:
+            return True
+
+    "Fill box if a square is drawn"
+    def fill_box(self, player, cell_1, cell_2):
+        board = self.board
+        "Format cell 1"
+        cell_1_point_1 = cell_1[0] * 2
+        cell_1_point_2 = cell_1[1] * 2
+
+        "Format cell 2"
+        cell_2_point_1 = cell_2[0] * 2
+        cell_2_point_2 = cell_2[1] * 2
+
+        "If last played line is horizontal, check square above and square below"
+        if cell_1_point_1 == cell_2_point_1:
+            played_line = (cell_1_point_1, (max(cell_1_point_2, cell_2_point_2) - 1))
+            condition_count = 0
+            "Check square above"
+            try:
+                if (
+                        board[played_line[0] - 1][played_line[1] - 1] == self.v_line and
+                        board[played_line[0] - 2][played_line[1]] == self.h_line and
+                        board[played_line[0] - 1][played_line[1] + 1] == self.v_line
+                ):
+                    "If they are all filled, draw the player's letter in the middle"
+                    board[played_line[0] - 1][played_line[1]] = player
+                    condition_count += 1
+                    "Adds a point if box is filled"
+                    if player == "A":
+                        self.score_player_A += 1
+                    if player == "B":
+                        self.score_player_B += 1
+            except IndexError:
+                pass
+            "check square below"
+            try:
+                if (
+                        board[played_line[0] + 1][played_line[1] + 1] == self.v_line and
+                        board[played_line[0] + 2][played_line[1]] == self.h_line and
+                        board[played_line[0] + 1][played_line[1] - 1] == self.v_line
+                ):
+                    board[played_line[0] + 1][played_line[1]] = player
+                    condition_count += 1
+                    if player == "A":
+                        self.score_player_A += 1
+                    if player == "B":
+                        self.score_player_B += 1
+            except IndexError:
+                pass
+            if condition_count > 0:
+                return True
+
+        "If last played line is vertical, check right and left squares"
+        if cell_1_point_2 == cell_2_point_2:
+            played_line = (max(cell_1_point_1, cell_2_point_1) - 1, cell_1_point_2)
+            condition_count = 0
+            "check square to the right"
+            try:
+                if (
+                        board[played_line[0] - 1][played_line[1] + 1] == self.h_line and
+                        board[played_line[0]][played_line[1] + 2] == self.v_line and
+                        board[played_line[0] + 1][played_line[1] + 1] == self.h_line
+                ):
+                    "If they are all filled, draw the player's letter in the middle"
+                    board[played_line[0]][played_line[1] + 1] = player
+                    condition_count += 1
+                    if player == "A":
+                        self.score_player_A += 1
+                    if player == "B":
+                        self.score_player_B += 1
+            except IndexError:
+                pass
+            "check square to the left"
+            try:
+                if (
+                        board[played_line[0] + 1][played_line[1] - 1] == self.h_line and
+                        board[played_line[0]][played_line[1] - 2] == self.v_line and
+                        board[played_line[0] - 1][played_line[1] - 1] == self.h_line
+                ):
+                    board[played_line[0]][played_line[1] - 1] = player
+                    condition_count += 1
+                    if player == "A":
+                        self.score_player_A += 1
+                    if player == "B":
+                        self.score_player_B += 1
+            except IndexError:
+                pass
+            if condition_count > 0:
+                return True
+
+    "Counts the number of plays"
+    def count_play(self):
+        self.play_count += 1
+        return self.play_count
+
+    "Game ends when the board is full"
+    def board_is_full(self):
+        play_count = self.play_count
+
+        "If x is the board size, the board will be full after 2(x^2 - x) plays"
+        if play_count == (((self.size_selection - 1) * self.size_selection) * 2):
+            return True
+        else:
+            return False
+
+    "Returns winner/tie"
+    def get_winner(self):
+        if self.score_player_A > self.score_player_B:
+            print("Player A wins!")
+        elif self.score_player_B > self.score_player_A:
+            print("Player B wins!")
+        else:
+            print("It's a tie!")
+
+    "Resets game"
+    def reset_game(self):
+        reset = input("Play again? (Y/N)").upper()
+        while reset not in ['Y', 'N']:
+            reset = input("Not valid. Play again? enter 'Y' or 'N')").upper()
+
+        if reset == "Y":
+            return True
+
+        if reset == "N":
+            return False
+
+"Creating player class"
+class Player:
+
+    "Initialises player 1 and player 2"
+    def __init__(self, player):
+
+        "Initialises symbols"
+        self.player = player
+        self.play = "o"
+        self.h_line = "—"
+        self.v_line = "|"
+        self.score = 0
+        self.line_played = None
+
+    "Moves the selected player"
+    def move_player(self, gameboard, cell_1, cell_2):
+        "Cell 1"
+        cell_1_point_1 = cell_1[0] * 2
+        cell_1_point_2 = cell_1[1] * 2
+
+        "Cell 2"
+        cell_2_point_1 = cell_2[0] * 2
+        cell_2_point_2 = cell_2[1] * 2
+
+        "Creates variable board"
+        board = gameboard.board
+
+        "logs the move"
+        board[cell_1_point_1][cell_1_point_2] = self.play
+        board[cell_2_point_1][cell_2_point_2] = self.play
+
+        "draws line between cells"
+        "if cells are horizontally adjacent draw an horizontal line"
+        if cell_1_point_1 == cell_2_point_1:
+            board[cell_1_point_1][(max(cell_1_point_2, cell_2_point_2) - 1)] = self.h_line
+
+        "if cells are vertically adjacent draw a vertical line"
+        if cell_1_point_2 == cell_2_point_2:
+            board[max(cell_1_point_1, cell_2_point_1) - 1][cell_1_point_2] = self.v_line
+
+    "Player vs computer"
--- main.py
+++ main.py
@@ -1,68 +1,151 @@
 # Importing classes from the classes.py file
-from classes import Board
-from classes import Player
+from classes import Board, Player
 import ast
 import random
 
-"Input desired board size"
-range_list = ["small", "medium", "large", "custom"] + [str(r) for r in range(3, 26)]
-size_selection = input("Enter board size \nSmall (4x4), Medium (9x9), Large (15x15) or Custom (x by x, min=3, max=25): ")
-
-"Input must be a valid range"
-while size_selection not in range_list:
-    print("Not valid")
-    size_selection = input("Enter board size \nmall (4x4), Medium (9x9), Large (15x15) or Custom (x by x, min=3, max=25): ").lower()
-
-"Select custom size if option is chosen"
-if size_selection not in ["small", "medium", "large"]:
-    if size_selection == "custom":
-        custom = int(input("Choose size x (x by x): "))
-        while custom not in list(range(3, 26)):
-            print("Not valid")
-            custom = int(input("Choose size x (x by x): "))
-    else:
-        custom = int(size_selection)
-        size_selection = "custom"
-else:
-    custom = 0
-
 "Show game example"
-print("\nChoose two cardinal points to make a move (x1, y1) (x2, y2)", "Example: (0,1) (0,2)", sep="\n")
 example = Board()
 example.example()
 
-"Creates player list"
-player_A = "A"
-player_B = "B"
-player_list = [player_A, player_B]
-p1 = Player()
-p2 = Player()
-
-"Run the game"
+"Input board size"
 while True:
-    "Generates the game board"
-    board = Board()
-    board.create_board(size_selection="small", custom="custom")
-    board.display_board()
+    try:
+        size_selection = int(input("Enter board size x as integer (min 3, max 15): "))
+        break
+    except ValueError:
+        print("Not valid. ", end="")
+        continue
 
-    "Randomizes first move"
+while size_selection < 3:
+    size_selection = int(input("Minimum board size is 3. Enter board size x as integer (min 3, max 15): "))
+
+while size_selection > 15:
+    size_selection = int(input("Maximum board size is 15. Enter board size x as integer (min 3, max 15): "))
+
+"Starts the game"
+while True:
+
+    "Creates player instances"
+    player_list = ["A", "B"]
     random.shuffle(player_list)
-    player_1 = player_list[0]
-    player_2 = player_list[1]
-    print(f"Player {player_1} moves first!")
+    letter_1 = player_list[0]
+    letter_2 = player_list[1]
+    player_1 = Player(player=letter_1)
+    player_2 = Player(player=letter_2)
+    play_count = 0
 
-    # while True:
-    #     "Logs move for each player in tuples"
-    #     try:
-    #         cell_1 = ast.literal_eval(input("Enter first point 'x1, y1': "))
-    #         while not board.is_cell_empty(cell=cell_1):
-    #             cell_1 = ast.literal_eval(input("Enter first point 'x1, y1': "))
-    # break
+    "Initialises the board"
+    gameboard = Board()
+    gameboard.create_board(size_selection=size_selection)
 
+    "Displays the board"
+    gameboard.display_board()
+    gameboard.get_score()
+    print(f"Player {letter_1} plays first!")
 
+    "Run the game"
+    while True:
 
+        turn = 0
+        "Player 1 moves"
+        while turn == 0 and gameboard.board_is_full() == False:
 
+            "Enter cells and checks for errors"
+            check_error = 1
+            while True:
+                while check_error == 1:
+                    try:
+                        cell_1 = ast.literal_eval(input(f"Player {letter_1}, enter first cell 'x1, y1': "))
+                        while cell_1[0] > (size_selection - 1) or cell_1[1] > (size_selection - 1) or cell_1[0] < 0 or cell_1[1] < 0:
+                            cell_1 = ast.literal_eval(input(f"Coordinate has to be in range between 0 and {size_selection - 1}. Enter again 'x1, y1': "))
+                        check_error = 2
+                        break
+                    except (ValueError, SyntaxError, TypeError):
+                        print("Not valid. ", end="")
+                        continue
 
+                while check_error == 2:
+                    try:
+                        cell_2 = ast.literal_eval(input(f"Player {letter_1}, enter second cell 'x1, y1': "))
+                        while cell_2[0] > (size_selection - 1) or cell_2[1] > (size_selection - 1) or cell_2[0] < 0 or cell_2[1] < 0:
+                            cell_2 = ast.literal_eval(input(f"Coordinate has to be in range between 0 and {size_selection - 1}. Enter again 'x1, y1': "))
+                        check_error = 0
+                        break
+                    except (ValueError, SyntaxError, TypeError):
+                        print("Not valid. ", end="")
+                        continue
 
+                "Checks the rules"
+                if not gameboard.check_rules(cell_1=cell_1, cell_2=cell_2):
+                    check_error = 1
+                else:
+                    break
 
+            "Logs move and displays board"
+            player_1.move_player(gameboard=gameboard, cell_1=cell_1, cell_2=cell_2)
+            gameboard.count_play()
+            if gameboard.fill_box(player=letter_1, cell_1=cell_1, cell_2=cell_2):
+                turn = 0
+            else:
+                turn = 1
+            gameboard.display_board()
+            gameboard.get_score()
 
+        "Player 2 moves"
+        while turn == 1 and gameboard.board_is_full() == False:
+
+            "Enter cells and checks for errors"
+            check_error = 1
+            while True:
+                while check_error == 1:
+                    try:
+                        cell_1 = ast.literal_eval(input(f"Player {letter_2}, enter first cell 'x1, y1': "))
+                        while cell_1[0] > (size_selection - 1) or cell_1[1] > (size_selection - 1) or cell_1[0] < 0 or cell_1[1] < 0:
+                            cell_1 = ast.literal_eval(input(f"Coordinate has to be in range between 0 and {size_selection - 1}. Enter again 'x1, y1': "))
+                        check_error = 2
+                        break
+                    except (ValueError, SyntaxError, TypeError):
+                        print("Not valid. ", end="")
+                        continue
+
+                while check_error == 2:
+                    try:
+                        cell_2 = ast.literal_eval(input(f"Player {letter_2}, enter second cell 'x1, y1': "))
+                        while cell_2[0] > (size_selection - 1) or cell_2[1] > (size_selection - 1) or cell_2[0] < 0 or cell_2[1] < 0:
+                            cell_2 = ast.literal_eval(input(f"Coordinate has to be in range between 0 and {size_selection - 1}. Enter again 'x1, y1': "))
+                        check_error = 0
+                        break
+                    except (ValueError, SyntaxError, TypeError):
+                        print("Not valid. ", end="")
+                        continue
+
+                "Checks the rules"
+                if not gameboard.check_rules(cell_1=cell_1, cell_2=cell_2):
+                    check_error = 1
+                else:
+                    break
+
+            "Logs move and displays board"
+            player_2.move_player(gameboard=gameboard, cell_1=cell_1, cell_2=cell_2)
+            gameboard.count_play()
+            if gameboard.fill_box(player=letter_2, cell_1=cell_1, cell_2=cell_2):
+                turn = 1
+            else:
+                turn = 0
+            gameboard.display_board()
+            gameboard.get_score()
+
+        "Checks if the board is full"
+        if gameboard.board_is_full():
+
+            "If yes display the winner ot tie"
+            gameboard.get_winner()
+            break
+        else:
+            continue
+
+    "Reset game"
+    if gameboard.reset_game():
+        continue
+    else:
+        break
