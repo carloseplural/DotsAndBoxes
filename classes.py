5ae4e8802a1ddb7a1657fa4263954f6dcadf8f44
Carlovila <carlos.gvferreira@gmail.com>
2022-12-16 22:40:22 +0000

    Added synchronous mode

--- classes.py
+++ classes.py
@@ -20,6 +20,7 @@ class Board:
         self.square_below = False
         self.square_right = False
         self.square_left = False
+        self.last_point = None
 
     "Creates the board based on a given input size"
     def create_board(self, size_selection):
@@ -264,6 +265,9 @@ class Board:
                     self.score_player_A += 1
                 if player == "B":
                     self.score_player_B += 1
+                if player == "S":
+                    self.score_player_A += 0.5
+                    self.score_player_B += 0.5
             pass
 
             "Check below"
@@ -277,6 +281,9 @@ class Board:
                     self.score_player_A += 1
                 if player == "B":
                     self.score_player_B += 1
+                if player == "S":
+                    self.score_player_A += 0.5
+                    self.score_player_B += 0.5
             pass
 
         "If last played line is vertical, check above and below squares"
@@ -294,6 +301,9 @@ class Board:
                     self.score_player_A += 1
                 if player == "B":
                     self.score_player_B += 1
+                if player == "S":
+                    self.score_player_A += 0.5
+                    self.score_player_B += 0.5
             pass
 
             "Check left"
@@ -307,8 +317,10 @@ class Board:
                     self.score_player_A += 1
                 if player == "B":
                     self.score_player_B += 1
+                if player == "S":
+                    self.score_player_A += 0.5
+                    self.score_player_B += 0.5
             pass
-
 
     "Counts the number of plays"
     def count_play(self):
@@ -346,7 +358,6 @@ class Board:
         if reset == "N":
             return False
 
-
 "Creating player class"
 class Player:
     def __init__(self, player, gameboard):
@@ -364,6 +375,9 @@ class Player:
         self.board_x = (len(self.gameboard.board) + 1) // 2
         self.board_y = (len(self.gameboard.board[0]) + 1) // 2
 
+        "Last played computer cells for synchronous mode"
+        self.last_computer_cell_1 = None
+        self.last_computer_cell_2 = None
 
 "Player Class"
 class Human(Player):
@@ -394,14 +408,159 @@ class Human(Player):
         if cell_1_point_2 == cell_2_point_2:
             board[max(cell_1_point_1, cell_2_point_1) - 1][cell_1_point_2] = self.v_line
 
+class Synchronous(Player):
+    
+    def __init__(self, gameboard):
+        super().__init__(player="", gameboard=gameboard)
+
+    def move_synchronous(self, player_cell_1, player_cell_2):
+        board_class = self.gameboard
+        board = self.gameboard.board
+        board_x = self.board_x
+        board_y = self.board_y
+        possible_moves = []
+        allowed_moves = []
+        best_moves = []
+
+        "Computer selects a move"
+        "Creates list of all possible moves in the board"
+        for x in range(board_x):
+            for y in range(board_y):
+                possible_moves.append((x, y))
+
+        "Creates list of all allowed moves left in the board"
+        for p1 in possible_moves:
+            for p2 in possible_moves:
+                if (
+                        (board_class.cells_are_empty(cell_1=p1, cell_2=p2)) and
+                        (board_class.cells_are_adjacent(cell_1=p1, cell_2=p2)) and
+                        (p1 != p2)
+                ):
+                    if (p2, p1) not in allowed_moves:
+                        allowed_moves.append((p1, p2))
+                    pass
+                pass
+
+        "Creates a sorted list of moves by potential points"
+        for a in allowed_moves:
+            board_class.check_box(cell_1=a[0], cell_2=a[1])
+            if board_class.condition_count == 2:
+                best_moves.append(a)
+            pass
+
+        for a in allowed_moves:
+            board_class.check_box(cell_1=a[0], cell_2=a[1])
+            if board_class.condition_count == 1:
+                best_moves.append(a)
+            pass
+
+        "Select move"
+        try:
+            computer_cell_1 = best_moves[0][0]
+            computer_cell_2 = best_moves[0][1]
+        except IndexError:
+            random_play = random.choice(allowed_moves)
+            computer_cell_1 = random_play[0]
+            computer_cell_2 = random_play[1]
+
+        "Formats computer cells"
+        computer_cell_1_point_1 = computer_cell_1[0] * 2
+        computer_cell_1_point_2 = computer_cell_1[1] * 2
+        computer_cell_2_point_1 = computer_cell_2[0] * 2
+        computer_cell_2_point_2 = computer_cell_2[1] * 2
+
+        "Player selects a move"
+        if (
+                (player_cell_1 == computer_cell_1 or player_cell_1 == computer_cell_2) and
+                (player_cell_2 == computer_cell_1 or player_cell_2 == computer_cell_2) and
+                (board_class.check_box(player_cell_1, player_cell_2) == True)
+        ):
+            "If the move is the same as the PC (which is always valid) and it fills a box, then point is split (S) "
+            board_class.fill_box(player="S", cell_1=player_cell_1, cell_2=player_cell_2)
+
+            "Draws a line"
+            if computer_cell_1_point_1 == computer_cell_2_point_1:
+                board[computer_cell_1_point_1][(max(computer_cell_1_point_2, computer_cell_2_point_2) - 1)] = self.h_line
+            if computer_cell_1_point_2 == computer_cell_2_point_2:
+                board[max(computer_cell_1_point_1, computer_cell_2_point_1) - 1][computer_cell_1_point_2] = self.v_line
+
+            "Counts the play"
+            board_class.count_play()
+            return True
+        elif (
+                (player_cell_1 == computer_cell_1 or player_cell_1 == computer_cell_2) and
+                (player_cell_2 == computer_cell_1 or player_cell_2 == computer_cell_2) and
+                (board_class.check_box(player_cell_1, player_cell_2) != True)
+        ):
+
+            "If the move is the same as the PC and it does not fill a box, returns false to main"
+            print("The computer picked the same line but it doesn't fill a box! Try again.")
+            return False
+        else:
+            "If neither case is true, computer and player move independently and player must respect rules"
+            if board_class.check_rules(player_cell_1, player_cell_2) == True:
+
+                "Computer makes the move"
+                "logs the move"
+                board[computer_cell_1_point_1][computer_cell_1_point_2] = self.play
+                board[computer_cell_2_point_1][computer_cell_2_point_2] = self.play
+
+                "draws line between cells"
+                "if cells are horizontally adjacent draw an horizontal line"
+                if computer_cell_1_point_1 == computer_cell_2_point_1:
+                    board[computer_cell_1_point_1][(max(computer_cell_1_point_2, computer_cell_2_point_2) - 1)] = self.h_line
+
+                "if cells are vertically adjacent draw a vertical line"
+                if computer_cell_1_point_2 == computer_cell_2_point_2:
+                    board[max(computer_cell_1_point_1, computer_cell_2_point_1) - 1][computer_cell_1_point_2] = self.v_line
+
+                "Check if move draws a box"
+                if board_class.check_box(cell_1=computer_cell_1, cell_2=computer_cell_2):
+                    board_class.fill_box(player="B", cell_1=computer_cell_1, cell_2=computer_cell_2)
+
+                "Counts the play"
+                board_class.count_play()
+
+                "Player makes the move"
+                "Formats cell 1"
+                player_cell_1_point_1 = player_cell_1[0] * 2
+                player_cell_1_point_2 = player_cell_1[1] * 2
+
+                "Formats cell 2"
+                player_cell_2_point_1 = player_cell_2[0] * 2
+                player_cell_2_point_2 = player_cell_2[1] * 2
+
+                "logs the move"
+                board[player_cell_1_point_1][player_cell_1_point_2] = self.play
+                board[player_cell_2_point_1][player_cell_2_point_2] = self.play
+
+                "draws line between cells"
+                "if cells are horizontally adjacent draw an horizontal line"
+                if player_cell_1_point_1 == player_cell_2_point_1:
+                    board[player_cell_1_point_1][(max(player_cell_1_point_2, player_cell_2_point_2) - 1)] = self.h_line
+
+                "if cells are vertically adjacent draw a vertical line"
+                if player_cell_1_point_2 == player_cell_2_point_2:
+                    board[max(player_cell_1_point_1, player_cell_2_point_1) - 1][player_cell_1_point_2] = self.v_line
+
+                "Check if move draws a box"
+                if board_class.check_box(cell_1=player_cell_1, cell_2=player_cell_2):
+                    board_class.fill_box(player="A", cell_1=player_cell_1, cell_2=player_cell_2)
+
+                "Counts the play"
+                board_class.count_play()
+
+                return True
+            else:
+                "If rules are not respected, both player and PC start again. (To avoid PC from logging a move in the board)"
+                return False
+
 "Computer Class"
 class Computer(Player):
 
     "Initialises methods"
     def __init__(self, player, gameboard):
         super().__init__(player, gameboard)
-        self.fill_box = None
-
 
 "Class random computer"
 class Random_Computer(Computer):
@@ -468,7 +627,7 @@ class Random_Computer(Computer):
 class Smart_Computer(Computer):
 
     "Computer selects and makes the move"
-    def move_computer(self):
+    def move_computer(self, mode):
         board_class = self.gameboard
         board = self.gameboard.board
         board_x = self.board_x
@@ -543,7 +702,3 @@ class Smart_Computer(Computer):
             self.fill_box = True
         else:
             self.fill_box = False
-
-
-
-
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 # Importing classes from the classes.py file
-from classes import Board, Human, Smart_Computer, Random_Computer
+from classes import Board, Human, Smart_Computer, Random_Computer, Synchronous
 import ast
 import random
 
@@ -363,53 +363,58 @@ while mode == "4":
     gameboard.create_board(size_selection=size_selection)
 
     "Creates player instances"
-    player = Human(gameboard=gameboard, player="A")
-    computer = Smart_Computer(gameboard=gameboard, player="B")
+    synchronous = Synchronous(gameboard=gameboard)
 
     "Displays the board"
     gameboard.display_board()
     gameboard.get_score()
     print("Players play simultaneously!")
 
-    "Run the game"
+    "Players moves"
     while True:
 
-        "Player 1 moves"
-        while gameboard.board_is_full() == False:
+        "Enter cells and checks for errors"
+        check_error = 1
+        while check_error == 1:
+            try:
+                cell_1 = ast.literal_eval(input(f"Enter first cell 'x1, y1': "))
+                while cell_1[0] > (selection_x - 1) or cell_1[1] > (selection_y - 1) or cell_1[0] < 0 or cell_1[1] < 0:
+                    cell_1 = ast.literal_eval(input(f"Coordinate not in range. Enter again 'x1, y1': "))
+                check_error = 2
+                break
+            except (ValueError, SyntaxError, TypeError, IndexError):
+                print("Not valid. ", end="")
+                continue
 
-            "Enter cells and checks for errors"
-            check_error = 1
-            while True:
-                while check_error == 1:
-                    try:
-                        cell_1 = ast.literal_eval(input(f"Enter first cell 'x1, y1': "))
-                        while cell_1[0] > (selection_x - 1) or cell_1[1] > (selection_y - 1) or cell_1[0] < 0 or cell_1[1] < 0:
-                            cell_1 = ast.literal_eval(input(f"Coordinate not in range. Enter again 'x1, y1': "))
-                        check_error = 2
-                        break
-                    except (ValueError, SyntaxError, TypeError, IndexError):
-                        print("Not valid. ", end="")
-                        continue
+        while check_error == 2:
+            try:
+                cell_2 = ast.literal_eval(input(f"Enter second cell 'x1, y1': "))
+                while cell_2[0] > (selection_x - 1) or cell_2[1] > (selection_y - 1) or cell_2[0] < 0 or cell_2[1] < 0:
+                    cell_2 = ast.literal_eval(input(f"Coordinate not in range. Enter again 'x1, y1': "))
+                check_error = 0
+                break
+            except (ValueError, SyntaxError, TypeError, IndexError):
+                print("Not valid. ", end="")
+                continue
 
-                while check_error == 2:
-                    try:
-                        cell_2 = ast.literal_eval(input(f"Enter second cell 'x1, y1': "))
-                        while cell_2[0] > (selection_x - 1) or cell_2[1] > (selection_y - 1) or cell_2[0] < 0 or cell_2[1] < 0:
-                            cell_2 = ast.literal_eval(input(f"Coordinate not in range. Enter again 'x1, y1': "))
-                        check_error = 0
-                        break
-                    except (ValueError, SyntaxError, TypeError, IndexError):
-                        print("Not valid. ", end="")
-                        continue
+        "Logs move"
+        synchronous.move_synchronous(player_cell_1=cell_1, player_cell_2=cell_2)
 
-                "Checks the rules"
-                if not gameboard.check_rules(cell_1=cell_1, cell_2=cell_2):
-                    check_error = 1
-                else:
-                    break
+        "Display board and score"
+        print("")
+        gameboard.display_board()
+        gameboard.get_score()
 
-            "Logs move"
-            player.move_player(cell_1=cell_1, cell_2=cell_2)
+        "Checks if the board is full"
+        if gameboard.board_is_full():
+            "If yes display the winner or tie"
+            gameboard.get_winner()
+            break
+        else:
+            continue
 
-            "Adds one play to the total"
-            gameboard.count_play()
+    "Reset game"
+    if gameboard.reset_game():
+        continue
+    else:
+        break
